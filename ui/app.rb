57a1c0c81327c741aed3afcdf44970bc44675d6c
Meck <yesmeck@gmail.com>
2015-01-24 00:05:21 +0800

    Add a web ui

---
+++ ui/app.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+require 'json'
+
+db = SQLite3::Database.new File.join(Dir.home, '.git-graph', 'data')
+
+get '/' do
+  erb :index
+end
+
+get '/commits' do
+  @commits = {}
+  db.execute('select * from commits').each do |row|
+    timestamp = Time.parse(row[2]).to_i
+    if @commits[timestamp]
+      @commits[timestamp] += 1
+    else
+      @commits[timestamp] = 1
+    end
+  end
+  JSON.generate(@commits)
+end
